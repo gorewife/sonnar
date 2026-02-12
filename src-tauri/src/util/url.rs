e9fb850623b05711863308b9e0089b9fd8c210c7
Power-el-Tanke <moiseselpower@protonmail.com>
2026-02-12 11:12:20 -0500

    feature(url): reqwest url wrapper

--- src-tauri/src/util/url.rs
+++ src-tauri/src/util/url.rs
@@ -1,4 +1,27 @@
+use reqwest;
+
 pub trait Url {
     fn host(&self) -> Option<&str>;
-    fn full_string(&self) -> &str;
+        fn full_string(&self) -> &str;
 }
+
+struct ReqwestUrl(reqwest::Url)
+
+impl ReqwestUrl {
+    #[inline]
+    pub fn inner(&self) -> reqwest::Url {
+                        &self.0
+    }
+}
+
+impl Url for ReqwestUrl{
+    fn host(&self) -> Option<&str> {
+        self.0.host_str
+}
+            
+    #[inline]
+    fn full_string(&self) -> &str {
+        self.0.as_str
+    }
+}
+
