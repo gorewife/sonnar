c7db0f5c992bd963e74881f8789258fbf40e7255
hystericca <viktor@hystericca.dev>
2026-03-16 18:10:44 -0700

    feat: bootstrap downloads store on mount

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
 import MainWindow from '@/layouts/MainWindow.vue'
+import { useDownloadsStore } from '@/stores/DownloadsStore'
+
+const downloads = useDownloadsStore()
+onMounted(() => downloads.init())
 </script>
 
 <template>
