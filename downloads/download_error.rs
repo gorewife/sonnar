37b733fc71bf32b00141e2b6d0110f759c85d6f1
Power-el-Tanke <moiseselpower@protonmail.com>
2026-03-23 02:00:00 +0000

    fix: deleted duplicated files

--- downloads/download_error.rs
+++
@@ -1,7 +0,0 @@
-use thiserror.Error;
-
-#[derive(Error, Debug)]
-pub enum DownloadError {
-    #[error("Lost connection with host of {0}")]
-    Disconnected(String),
-}
--- downloads/downloader.rs
+++
@@ -1,6 +0,0 @@
-use super::DownloadError;
-
-pub trait Downloader {
-    is_valid_url(&Url) -> bool;
-    async start_download(&Url) -> Result<Download, DownloadError>;
-}
